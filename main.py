78b9c318161b6f2370b77ed29ec01a19631954dd
Mak-2805 <makariossamwail@gmail.com>
2023-12-20 01:45:10 -0500

    Extract all relevent data and store them in temporary lists

--- main.py
+++ main.py
@@ -16,3 +16,29 @@ for page in range(2):
     data = requests.get(url).text
     soup = BeautifulSoup(data, "html.parser")
 
+    table = soup.find("table", id = "ContentPlaceHolder1_ProductClassDetail") #extract the table which holds all the chemicals
+
+    #initialize lists to hold relevent data
+    CAS = [len(table_rows)]
+    productName = [len(table_rows)]
+    molecularFormula = [len(table_rows)]
+
+    table_rows = table.find_all("a") #extracts all the rows of the table that use the anchor html tag (all the CAS and product names are wrapped in the anchor tag)
+
+
+    #extract and append all the CAS numbers and product names into their respective lists
+    for chemical in range(len(table_rows)):
+        if(chemical%2 == 0):
+            CAS.append(table_rows[chemical].text)
+            
+        else:
+            productName.append(table_rows[chemical].text)
+            
+    table_rows = table.find_all("span") #extracts all the rows of the table that use the span html tag (all the molecular formulas are wrapped in the span tag)
+    
+    #extract and append all the molecular formulas in its list
+    for chemical in range(len(table_rows)):
+        molecularFormula.append(table_rows[chemical].text)
+
+    for i in range(len(productName)):
+        print(CAS[i], productName[i], molecularFormula[i])
